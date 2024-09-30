59c24727b627fcb89a567950e72481d7065db20c
Balaji-Sai-charan <93989753+Balaji-Sai-charan@users.noreply.github.com>
2024-09-30 17:57:44 +0530

    Update Greetings.jsx

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,1 +1,57 @@
+import React, { useEffect, useState } from 'react';
 
+const greetings = [
+    "Hello",
+    "नमस्ते",
+    "வணக்கம்",
+    "ഹലോ",
+    "ಹಲೋ",
+    "Bonjour",
+    "Hola",
+    "Ciao",
+    "Howdy",
+    "Привет",
+    "你好",
+    "안녕하세요",
+    "こんにちは",
+    "Merhaba",
+    "Salam",
+    "Zdravo",
+    "నమస్కారం 🙏"
+];
+
+const Greeting = ({ onComplete }) => {
+    const [currentGreeting, setCurrentGreeting] = useState('');
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const showGreeting = () => {
+            if (index < greetings.length) {
+                setCurrentGreeting(greetings[index]);
+                let displayDuration = 80;
+
+                if (greetings[index] === "Hello") {
+                    displayDuration = 400;
+                } else if (greetings[index] === "నమస్కారం 🙏") {
+                    displayDuration = 500;
+                }
+
+                console.log(`Index: ${index}, Greeting: ${greetings[index]}`);
+                setIndex(index + 1);
+                setTimeout(showGreeting, displayDuration);
+            } else {
+                setTimeout(onComplete, 100);
+            }
+        };
+
+        showGreeting();
+    }, [index, onComplete]);
+
+    return (
+        <div id="greeting">
+            <h1>{currentGreeting}</h1>
+        </div>
+    );
+};
+
+export default Greeting;
